dfce1c6c8d567073a3f15492ea64fb7d53dbbb3a
Ishan <agarwal.ishanedu@gmail.com>
2026-03-06 11:27:53 -0500

    Created the rectangle file and added basic documentation

---
+++ rectangle.py
@@ -0,0 +1,13 @@
+"""
+Program Name: Lab 8 (rectangle.py)
+My name: Ishan Agarwal
+Purpose: This program is meant to deepen my understanding of functions in python
+Starter Code: None
+Date: 03/06/2026
+
+Functions: 
+ area(width, height): This function is used to calculate and return the area of a rectangle.
+ perimeter(width, height): This function is used to calculate and return the area of a rectangle. 
+"""
+
+import math
