d0d2a22982acea0b410a22df186f7d72b9f42759
Ishan <agarwal.ishanedu@gmail.com>
2026-03-06 11:15:57 -0500

    Revision: Create the file and add inital documentation

---
+++ circle.py
@@ -0,0 +1,14 @@
+"""
+Program Name: Lab 8 (Circle.py)
+My name: Ishan Agarwal
+Purpose: This program is meant to deepen my understanding of functions in python
+Starter Code: None
+Date: 03/06/2026
+
+Functions: 
+ area(radius): This function is used to calculate and return the area of a circle.
+ circumference(radius): This function is used to calculate and return the area of a circle. 
+"""
+
+import math
+
