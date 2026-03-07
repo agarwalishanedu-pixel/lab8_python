7000dfe580a8b204553d52c314cdd2fd51cc4661
Ishan <agarwal.ishanedu@gmail.com>
2026-03-06 23:21:56 -0500

    Create the main file and import the other files

---
+++ Lab8_iagarwal-1.py
@@ -0,0 +1,13 @@
+"""
+Program Name: Lab 8 (Main File)
+My name: Ishan Agarwal
+Purpose: This program is meant to deepen my understanding of functions in python. Also this is used to calcu
+Starter Code: None
+Date: 03/06/2026 
+"""
+
+# Importing the files with aliases
+# Using aliases are important especially because both of the files have a function called Area. 
+import circle as c
+import rectangle as r
+
