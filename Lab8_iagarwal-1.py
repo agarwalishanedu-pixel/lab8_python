8b111b91dbebca07ec0043f24b1e5fdf6f7ef3e2
Ishan <agarwal.ishanedu@gmail.com>
2026-03-07 00:03:40 -0500

    Final type hints + documentation

--- Lab8_iagarwal-1.py
+++ Lab8_iagarwal-1.py
@@ -28,40 +28,46 @@ while run:
     print("")
 
     # Evaluate their choice
+    # Calculate area of circle
     if option == "1":
-        radius = float(input("Enter the radius of the circle: "))
+        radius: float = float(input("Enter the radius of the circle: "))
         print("")
-        final_value = c.area(radius)
+        final_value: float = c.area(radius)
         print(f"The area of the circle is {final_value}.")
         print("")
     
+    # Calculate circumference of circle
     elif option == "2":
-        radius = float(input("Enter the radius of the circle: "))
+        radius: float = float(input("Enter the radius of the circle: "))
         print("")
-        final_value = c.circumference(radius)
+        final_value: float = c.circumference(radius)
         print(f"The circumference of the circle is {final_value}.")
         print("")
         
+    # Calculate area of rectangle 
     elif option == "3":
-        width = float(input("Enter the width of the rectangle: "))
-        height = float(input("Enter the height of the rectangle: "))
+        width: float = float(input("Enter the width of the rectangle: "))
+        height: float = float(input("Enter the height of the rectangle: "))
         print("")
-        final_value = r.area(width, height)
+        final_value: float = r.area(width, height)
         print(f"The area of the rectangle is {final_value}.")
         print("")
 
+    # Calculate perimeter of rectangle 
     elif option == "4":
-        width = float(input("Enter the width of the rectangle: "))
-        height = float(input("Enter the height of the rectangle: "))
+        width: float = float(input("Enter the width of the rectangle: "))
+        height: float = float(input("Enter the height of the rectangle: "))
         print("")
-        final_value = r.perimeter(width, height)
+        final_value: float = r.perimeter(width, height)
         print(f"The perimeter of the rectangle is {final_value}.")
         print("")
 
+    # Exit loop
     elif option == "5":
         print("Goodbye!")
         run = False
 
+    # Continuation message
     if run:
         input("Press Enter to continue...")
         print("")
